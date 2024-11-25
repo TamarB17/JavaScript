b2389a9b24baeefb355836013d74b80e6ceceaf6
TamarB17 <tamarbendeliani17@gmail.com>
2024-11-25 20:08:10 +0400

    testJS

---
+++ test.js
@@ -0,0 +1,63 @@
+//1
+
+let x=87
+let y=65
+console.log (x+y)
+
+//2
+
+let student = {
+    firstName: "ana",
+    lastName: "ragacashvili",
+    age: "ana@gmail.com",
+    marks: [99, 65, 88]
+    }
+console.log (student.firstName)
+
+//3
+
+let m=45 
+let n=23
+if (m>n) {
+    console.log (m+n)
+}
+else {
+    console.log (n)
+}
+
+//4
+
+let studentName = "tamar";
+console.log('Rise and shine, "' + studentName + '"!');
+
+//5
+
+let salary=3000;
+let bonus;
+
+if (salary >= 2000) {
+    bonus = 0;
+} else {
+    bonus = salary * 0,1;}
+     
+    console.log (bonus)
+
+    //6
+
+    let a=23;
+    let b=a/2;
+    let c=a+b;
+    console.log (c)
+
+    //7
+
+    let phones = ["iphone", "samsung","pixel","huawei","xiaomi" ]
+    console.log (phones)
+
+    //8
+
+    console.log (phones[0])
+    console.log (phones[4])
+
+
+
